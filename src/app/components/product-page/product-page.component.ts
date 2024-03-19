759cc9cf61614d4c5b15c4ec8795a6ed42a401ed
Andres Forero <anfforerove@gmial.com>
2024-03-18 22:42:08 -0500

    Terminando movile first

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -6,10 +6,13 @@ import { CarouselComponent } from '../../shered/carousel/carousel.component';
   standalone: true,
   imports: [CarouselComponent],
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.scss'
+  styleUrl: './product-page.component.scss',
 })
 export class ProductPageComponent {
-
-  images = []
-
+  images = [
+    '../../../assets/images/image-product-1.jpg',
+    '../../../assets/images/image-product-2.jpg',
+    '../../../assets/images/image-product-3.jpg',
+    '../../../assets/images/image-product-4.jpg',
+  ];
 }
