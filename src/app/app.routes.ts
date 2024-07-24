3c3cd338a714039bee323ba02f386f820b6c920f
Andres Forero <anfforerove@gmial.com>
2024-07-24 17:57:45 -0500

    Init page movile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductPageComponent } from './components/product-page/product-page.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { InicioPageComponent } from './components/inicio-page/inicio-page.component';
 
 export const routes: Routes = [
-    {path:'', component: MainPageComponent},
-    {path:':productID', component: ProductPageComponent}
+    {path:'', component: InicioPageComponent},
+    {path:'main', component: MainPageComponent},
+    {path:'main/:productID', component: ProductPageComponent}
 ];
---
+++ src/app/components/inicio-page/inicio-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './inicio-page.component.html',
+  styleUrl: './inicio-page.component.scss'
+})
+export class InicioPageComponent {
+
+}
